05d6441a66218d2b12fff32f4d53c818b9683cb2
Rocker <rocker@xxx.com>
2024-11-21 17:02:55 -0500

    Add user profile management

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    preferences = models.JSONField()  # Store chatbot preferences like tone
-    mental_health_goals = models.TextField()
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    age = models.IntegerField(null=True, blank=True)
+    preferences = models.JSONField(default=dict, blank=True)
+    mental_health_goals = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('update-profile/', views.update_profile, name='update_profile'),
 ]
