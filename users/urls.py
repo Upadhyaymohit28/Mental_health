9f08ee0eda050ce730189fe500fba9f60fcc73fe
Xu Gao <xgao106@uottawa.ca>
2024-11-23 00:15:50 -0500

    update for dashboard

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_user, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout_user, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('update-profile/', views.update_profile, name='update_profile'),
